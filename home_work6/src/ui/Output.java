7d482080299372edcb42805967ef650c903247d2
Danila Sukhorukov <suhorukovdanila@yandex.ru>
2023-01-23 13:27:42 +0300

    first commit

---
+++ home_work6/src/ui/Output.java
@@ -0,0 +1,14 @@
+package ui;
+
+import Model.Picture;
+
+public class Output {
+    Picture picture;
+
+    public Output(Picture picture) {
+        this.picture = picture;
+    }
+    public void showPicture(){
+        System.out.println(picture);
+    }
+}
